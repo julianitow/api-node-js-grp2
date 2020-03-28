5c5b0b3e816d36b9f5a97574dfbbe8c3d6ec89e5
Imane <imane@MacBook-Pro-de-Imane.local>
2020-03-28 12:15:42 +0100

    finalisation sign up

--- server.js
+++ server.js
@@ -7,32 +7,39 @@ const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:' + 27017 + '/notes-api' ;
 const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const DATABASE_NAME = 'notes-api';
-const COLLECTION_NAME = "users";
 require('dotenv').config()
 var privateKey = process.env.JWT_KEY
 
 app.post('/', (req, res) => {
     res.send("Bienvenue sur notre API");
 });
- 
- (async () => {
-   await client.connect();
-   console.log(`Connecting to ${DATABASE_NAME}`);
 
-   const collection = client.db(DATABASE_NAME).collection(COLLECTION_NAME);
-   const users = await collection.find({}).toArray();
-   console.log('users:', users) 
-   app.listen(PORT, function () {
-    console.log("Listening on port " + PORT);
-  })
-  // await client.close();
-  })();
+// BDD
+  (async function() {
+    try {
+      await client.connect();
+      console.log(`Connecting to ${DATABASE_NAME}`);
+    } catch (err) {
+       console.log(err.stack);
+       }
+    })();
 
+  async function getCol(colName) {
+    const db = client.db(DATABASE_NAME);
+    return db.collection(colName);
+  }
+
+  async function findUser(username){
+    const col = await getCol('users');
+    const user = await col.findOne({ username: username });
+    return user == null ? false : user; 
+  }
+   
   // Route POST /signup
+  app.post('/signup',async function (req, res, next) {
 
-  app.post('/signup',function(req, res, next) {
-    // console.log("inscription");
-    var regex = /azertyuiopqsdfghjklmwxcvbn/;
+     var user = await findUser(req.body.username);
+
       if (!req.body.username || !req.body.password) {
          var erreur = "Vous devez fournir un identifiant et un mot de passe";
          var token = null;
@@ -44,7 +51,7 @@ app.post('/', (req, res) => {
         var token = null;
       }
     
-      else if(!username.match(/^[a-z]+$/)){
+      else if(!req.body.username.match(/^[a-z]+$/)){
           var erreur = "Votre identifiant ne doit contenir que des lettres minuscules non accentuées";
           var code = 400;
           var token = null;
@@ -52,6 +59,13 @@ app.post('/', (req, res) => {
       }
       else if(req.body.username.length < 2 || req.body.username.length > 20){
         var erreur = "Votre identifiant doit contenir entre 2 et 20 caractères"
+        var code = 400
+        var token = null;
+      }
+
+
+      else if(user) {
+        var erreur = "Cet identifiant est déjà associé à un compte"
         var code = 400
         var token = null;
       }
@@ -63,6 +77,12 @@ app.post('/', (req, res) => {
           exp: Math.floor(Date.now() / 1000) + (60 * 60) * 24,
           username: req.body
         },privateKey);
+
+        const { insertedId } = await((await getCol('users')).insertOne({
+          username: req.body.username,
+          password: req.body.password
+        }));
+
       }
     
       return res.status(code).json({
@@ -76,4 +96,9 @@ app.post('/', (req, res) => {
   app.post('/signin',function(req,res, err){
     console.log("connexion");
   });
+
+  app.listen(3000, function () {
+    console.log('Listening on port 3000!')
+  })
+  
   
